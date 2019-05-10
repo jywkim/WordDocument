b5ed4000534d4b215daab7703272417ab1ed6f61
jywkim <jkim589@gmail.com>
2019-05-10 19:07:58 -0400

    create word doc

--- WordDocument/Form1.cs
+++ WordDocument/Form1.cs
@@ -19,7 +19,34 @@ namespace WordDocument
 
         private void button1_Click(object sender, EventArgs e)
         {
+            CreateDocument();
+        }
 
+        private void CreateDocument()
+        {
+            //Create instance for Word app
+            Microsoft.Office.Interop.Word.Application winword = new Microsoft.Office.Interop.Word.Application();
+
+            //Set animation status for Word application
+            winword.ShowAnimation = false;
+
+            //Set status for Word application is to be visible or not
+            winword.Visible = false;
+
+            //Create missing variable for missing value
+            object missing = System.Reflection.Missing.Value;
+
+            //Create new document
+            Microsoft.Office.Interop.Word.Document document = winword.Documents.Add(ref missing, ref missing, ref missing, ref missing);
+
+            //Save document
+            object filename = @"C:\Users\Jon\Desktop\test.docx";
+            document.SaveAs2(ref filename);
+            document.Close(ref missing, ref missing, ref missing);
+            document = null;
+            winword.Quit(ref missing, ref missing, ref missing);
+            winword = null;
+            MessageBox.Show("Document created successfully !");
         }
     }
 }
